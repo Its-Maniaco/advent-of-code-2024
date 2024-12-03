67d6c8bacba9d01e941550f47f8a1c9cb9a28023
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-12-03 14:28:23 +0530

    day3 part2

--- days/day3/day3.go
+++ days/day3/day3.go
@@ -32,6 +32,40 @@ func Part1(fileLoc string) {
 	fmt.Println(result)
 }
 
+func Part2(fileLoc string) {
+	err, lines := utils.LineSlice(fileLoc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exp := `mul\(\d+,\d+\)|don't\(\)|do\(\)`
+	matches := getMatches(lines, exp)
+
+	result := 0
+
+	var flag bool = true // to allow multiplying
+	// parse the combinations
+	for _, match := range matches {
+		if match == "do()" {
+			flag = true
+			continue
+		} else if match == "don't()" {
+			flag = false
+			continue
+		}
+
+		if flag == true {
+			s := strings.Split(match, ",")           // split around ','
+			a, _ := strconv.Atoi(s[0][4:])           // mul(....
+			b, _ := strconv.Atoi(s[1][:len(s[1])-1]) //.....)
+			result += a * b
+		}
+
+	}
+
+	fmt.Println(result)
+}
+
 func getMatches(lines []string, exp string) []string {
 	re, err := regexp.Compile(exp)
 	if err != nil {
