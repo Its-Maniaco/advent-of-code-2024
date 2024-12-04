242966c8aea1613c52c9ae8a9b46614d076fecba
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-12-04 14:40:13 +0530

    day4 refactor

--- days/day4/day4.go
+++ days/day4/day4.go
@@ -25,8 +25,26 @@ func Part1(fileLoc string) {
 					ans += checkInLine(line)
 					flag = true
 				}
-				ans += checkVertical(lines, i, j)
-				ans += checkDiagonal(lines, i, j)
+				ans += checkVertical(lines, "XMAS", i, j)
+				ans += checkDiagonal(lines, "XMAS", i, j)
+			}
+		}
+	}
+	fmt.Println(ans)
+}
+
+func Part2(fileLoc string) {
+	err, lines := utils.LineSlice(fileLoc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ans := 0
+
+	for i, line := range lines {
+		for j, ch := range line {
+			if string(ch) == "M" {
+				ans += checkDiagonal(lines, "MAS", i, j)
 			}
 		}
 	}
@@ -40,7 +58,7 @@ func checkInLine(line string) int {
 	return count
 }
 
-func checkVertical(mat []string, r int, c int) int {
+func checkVertical(mat []string, cmp string, r int, c int) int {
 	count := 0
 	var check string
 
@@ -48,54 +66,63 @@ func checkVertical(mat []string, r int, c int) int {
 	for k := r; k >= r-3 && k >= 0; k-- {
 		check = check + string(mat[k][c])
 	}
-	count = increaseIfMatch(check, count)
+	count = increaseIfMatch(check, cmp, count)
 
 	//below
 	check = ""
 	for k := r; k <= r+3 && k < len(mat); k++ {
 		check = check + string(mat[k][c])
 	}
-	count = increaseIfMatch(check, count)
+	count = increaseIfMatch(check, cmp, count)
 
 	return count
 }
 
-func checkDiagonal(mat []string, r int, c int) int {
+func checkDiagonal(mat []string, cmp string, r int, c int) int {
 	count := 0
 
-	// top left
+	count += checkDiagTopLeft(mat, cmp, r, c)
+	count += checkDiagTopRight(mat, cmp, r, c)
+	count += checkDiagBottomLeft(mat, cmp, r, c)
+	count += checkDiagBottomRight(mat, cmp, r, c)
+
+	return count
+}
+
+func checkDiagTopLeft(mat []string, cmp string, r int, c int) int {
 	check := ""
 	for i, j := r, c; i >= 0 && j >= 0 && i >= r-3 && j >= c-3; i, j = i-1, j-1 {
 		check = check + string(mat[i][j])
 	}
-	count = increaseIfMatch(check, count)
+	return increaseIfMatch(check, cmp, 0)
+}
 
-	// top right
-	check = ""
+func checkDiagTopRight(mat []string, cmp string, r int, c int) int {
+	check := ""
 	for i, j := r, c; i >= 0 && j < len(mat[0]) && i >= r-3 && j <= c+3; i, j = i-1, j+1 {
 		check = check + string(mat[i][j])
 	}
-	count = increaseIfMatch(check, count)
+	return increaseIfMatch(check, cmp, 0)
+}
 
-	// bottom left
-	check = ""
+func checkDiagBottomLeft(mat []string, cmp string, r int, c int) int {
+	check := ""
 	for i, j := r, c; i < len(mat) && j >= 0 && i <= r+3 && j >= c-3; i, j = i+1, j-1 {
 		check = check + string(mat[i][j])
 	}
-	count = increaseIfMatch(check, count)
+	return increaseIfMatch(check, cmp, 0)
+}
 
-	// bottom right
-	check = ""
+func checkDiagBottomRight(mat []string, cmp string, r int, c int) int {
+	check := ""
 	for i, j := r, c; i < len(mat) && j < len(mat[0]) && i <= r+3 && j <= c+3; i, j = i+1, j+1 {
 		check = check + string(mat[i][j])
 	}
-	count = increaseIfMatch(check, count)
-
-	return count
+	return increaseIfMatch(check, cmp, 0)
 }
 
-func increaseIfMatch(s string, count int) int {
-	if s == "XMAS" {
+func increaseIfMatch(s string, cmp string, count int) int {
+	if s == cmp {
 		return count + 1
 	}
 	return count
