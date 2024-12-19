f6599dbc061e0abe48dbeaa5d3a27b2aa3a0b1c4
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-12-19 20:33:45 +0530

    day19 part2

--- days/day19/day19.go
+++ days/day19/day19.go
@@ -20,12 +20,31 @@ func Part1(fileLoc string) {
 	}
 
 	count := 0
-	fmt.Println(dsgn)
-	fmt.Println(ptrn)
 	for _, row := range dsgn {
 		if checkPossible(row, ptrn, "") {
 			count++
 		}
+	}
+
+	fmt.Println("Ans: ", count)
+}
+
+func Part2(fileLoc string) {
+	err, fs := utils.LineSlice(fileLoc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ptrn, dsgn := getPatternDesign(fs)
+	if ptrn == nil || dsgn == nil {
+		log.Fatal("Empty desgin or pattern.")
+	}
+
+	count := 0
+	for _, row := range dsgn {
+		fmt.Println("Checking desgin: ", row)
+		memo := make(map[string]int)
+		count += countPossible(row, ptrn, memo, "")
 	}
 
 	fmt.Println("Ans: ", count)
@@ -45,6 +64,30 @@ func checkPossible(s string, ptrn []string, tmp string) bool {
 		}
 	}
 	return false
+}
+
+func countPossible(s string, ptrn []string, memo map[string]int, tmp string) int {
+	if tmp == s {
+		return 1
+	}
+
+	if !strings.HasPrefix(s, tmp) {
+		return 0
+	}
+
+	if val, found := memo[tmp]; found {
+		return val
+	}
+	count := 0
+
+	for i := 0; i < len(ptrn); i++ {
+		tmp2 := tmp + ptrn[i]
+		if strings.HasPrefix(s, tmp2) {
+			count += countPossible(s, ptrn, memo, tmp2)
+		}
+	}
+	memo[tmp] = count
+	return count
 }
 
 func getPatternDesign(fs []string) ([]string, []string) {
