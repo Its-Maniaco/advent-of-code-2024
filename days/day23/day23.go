875992b9581ea80f308262edd67e0f27a0db3c0f
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-12-23 19:42:44 +0530

    use slice instead of string to get valid conns

--- days/day23/day23.go
+++ days/day23/day23.go
@@ -18,8 +18,18 @@ func Part1(fileLoc string) {
 	lan := buildMap(fs)
 
 	validConns := connLanFind(lan)
-	director := startsWith(validConns, "t")
-	fmt.Println(len(director))
+
+	// add those connections which have a "t" in start
+	director := [][]string{}
+	for _, row := range validConns {
+		for _, s := range row {
+			if string(s[0]) == "t" {
+				director = append(director, row)
+				break
+			}
+		}
+	}
+	fmt.Println("Ans: ", len(director))
 }
 
 // TODO: try without 2 way keys
@@ -39,8 +49,9 @@ func buildMap(fs []string) map[string][]string {
 }
 
 // get all valid 3 way connections
-func connLanFind(lan map[string][]string) []string {
-	lans := []string{}
+func connLanFind(lan map[string][]string) [][]string {
+	lans := [][]string{}
+	lansString := []string{}
 	// k,v are base map key value
 	for k, v := range lan {
 		ss := []string{}
@@ -61,11 +72,11 @@ func connLanFind(lan map[string][]string) []string {
 					if k1 == k3 {
 						ss = append(ss, k, k2, k3)
 						sort.Strings(ss)
-						s := connString(ss)
-						if !checkConnExists(lans, s) {
-							lans = append(lans, s)
+						ssString := connString(ss)
+						if !checkConnStringExists(lansString, ssString) {
+							lans = append(lans, ss)
+							lansString = append(lansString, ssString)
 						}
-
 						ss = nil
 					}
 				}
@@ -76,27 +87,17 @@ func connLanFind(lan map[string][]string) []string {
 	return lans
 }
 
+// convert a connection into string
 func connString(s []string) string {
 	ss := fmt.Sprintf("%v-%v-%v", s[0], s[1], s[2])
 	return ss
 }
 
-func checkConnExists(lans []string, s string) bool {
+func checkConnStringExists(lans []string, s string) bool {
 	for _, v := range lans {
 		if s == v {
 			return true
 		}
 	}
 	return false
-}
-
-func startsWith(lans []string, ch string) []string {
-	starts := []string{}
-	for _, s := range lans {
-		//ab-cd-ef
-		if string(s[0]) == ch || string(s[3]) == ch || string(s[6]) == ch {
-			starts = append(starts, s)
-		}
-	}
-	return starts
 }
